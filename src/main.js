c6d47808ef1f56ad76b6384c95b13631f032aa6c
bertyn99 <bboulikou@gmail.com>
2020-08-23 19:15:46 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
